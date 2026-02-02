4eed20c86c3b9cfe4bdc5aba3016f1accec5bcf8
Darius Cupsa <cupsadarius@gmail.com>
2026-02-02 11:14:05 +0200

    release

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,12 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter()
+	},
+	package: {
+		// Exclude test files from the package
+		files: (filepath) => {
+			return !filepath.includes('__tests__');
+		}
 	}
 };
 
